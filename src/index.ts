2c4c1eb46d884dc7f4f606e3a211fc7b3ed8ea9e
anan_p <se56160422@gmail.com>
2021-01-26 10:23:23 +0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const handleBackToGGApp = () => {
+    // if (typeof (window as WindowWithPrintType).Print === "undefined") {
+    //   window.location.href = process.env.REACT_APP_GG_APP_URL as string;
+    //   return;
+    // }
+    window.postMessage("go_to_gg_app","");
+  };
+
+  
